71c79a09febae25b82d4eed91798322316dca356
can <omercansoyu@gmail.com>
2023-06-20 16:16:31 +0300

    empty

--- app/page.js
+++ app/page.js
@@ -3,7 +3,9 @@ import "./globals.css";
 export default function Home() {
   return (
     <div className="HomeCont">
-      <h1>Home Page</h1>
+      <div>
+        <h1>Home Page</h1>
+      </div>
     </div>
   );
 }
--- components/Header.js
+++ components/Header.js
@@ -6,6 +6,8 @@ const Header = () => {
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
       <Link href="/contact">Contact</Link>
+      <Link href="/">Products</Link>
+      <Link href="/about">Chairmains</Link>
     </div>
   );
 };
